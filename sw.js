46b800e49f330c25269074961db6febeb4821e5c
Alee <zhousichuan@hongtao-edu.com>
2018-09-08 15:09:54 +0800

    修改

--- sw.js
+++ sw.js
@@ -10,8 +10,8 @@ const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "aleej.com",
+  "drowtram.github.io",
   "cdnjs.cloudflare.com"
 ]
 
